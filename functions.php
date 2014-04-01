cad8ae44c7faf347bbac165f75bad8766ab5d733
Adam Onishi <onishiweb@gmail.com>
2014-04-01 20:57:40 +0100

    Add footer advert and menus, set up functions file

--- functions.php
+++ functions.php
@@ -1,8 +1,73 @@
 <?php
 
+if ( ! function_exists( 'adamonishi_setup' ) ):
 
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which runs
+	 * before the init hook. The init hook is too late for some features, such as indicating
+	 * support post thumbnails.
+	 */
+	function adamonishi_setup() {
+
+		/**
+		 * Theme supports: Feed links in head, post formats (aside/image/gallery), post thumbnails...
+		 */
+		add_theme_support( 'automatic-feed-links' );
+		// add_theme_support( 'post-formats', array( 'aside', 'image', 'gallery' ) );
+		add_theme_support( 'post-thumbnails' );
+
+		/**
+		 * This theme uses wp_nav_menu() in one location.
+		 */
+		register_nav_menus( array(
+			'external-links' => __( 'External links', 'adamonishi' ),
+			'other-projects' => __( 'Other projects', 'adamonishi' ),
+		) );
+
+		/**
+		 * Remove the crap from the wp_head() function
+		 */
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'feed_links', 2);
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'feed_links_extra', 3);
+		remove_action('wp_head', 'start_post_rel_link', 10, 0);
+		remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+		remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+
+		/**
+		 * Remove any unwanted wordpress dashboard boxes
+		 */
+		function disable_default_dashboard_widgets() {
+			remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
+			remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
+			remove_meta_box('dashboard_plugins', 'dashboard', 'core');
+			remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
+		}
+		add_action('admin_menu', 'disable_default_dashboard_widgets');
+
+	}
+
+endif; // adamonishi_setup
+/**
+ * Tell WordPress to run toolbox_setup() when the 'after_setup_theme' hook is run.
+ */
+add_action( 'after_setup_theme', 'adamonishi_setup' );
 
 function ao_enqueue_styles() {
+	/**
+	 * Better jQuery inclusion
+	 */
+	if (!is_admin()) {
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"), false);
+		wp_enqueue_script('jquery');
+	}
+
 	wp_enqueue_style( 'main', get_stylesheet_uri() );
 }
 
--- index.php
+++ index.php
@@ -41,17 +41,59 @@
 
 			<?php endwhile; endif; ?>
 
-			<footer class="global-footer">
-				
-			</footer>
+			<nav class="page-nav clearfix">
+				<div class="align-left">
+					<?php next_posts_link( '&laquo; previous posts'); ?>
+				</div>
+				<div class="align-right">
+					<?php previous_posts_link( 'next posts &raquo;' ); ?>
+				</div>
+			</nav>
 		</div>
 
+		<footer class="global-footer">
+			<div class="container clearfix">
+				<div class="footer-col">
+					<h2>Buy now!</h2>
+					<a href="http://www.amazon.co.uk/gp/product/1430259140/ref=as_li_ss_il?ie=UTF8&camp=1634&creative=19450&creativeASIN=1430259140&linkCode=as2&tag=adamonishicom-21" class="book-advert">
+						<img border="0" src="http://ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=1430259140&Format=_SL250_&ID=AsinImage&MarketPlace=GB&ServiceVersion=20070822&WS=1&tag=adamonishicom-21" >
+						Buy Pro WordPress Theme Development on&nbsp;Amazon
+					</a>
+					<img src="http://ir-uk.amazon-adsystem.com/e/ir?t=adamonishicom-21&l=as2&o=2&a=1430259140" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+				</div>
+				<div class="footer-col">
+					<h2>Find me on&hellip;</h2>
+					<?php
+						$args = array(
+							'theme_location' => 'external-links',
+							'container' => '',
+							'menu_class' => 'menu',
+						);
+					
+						wp_nav_menu( $args );
+					?>
+					<h2>Other projects&hellip;</h2>
+					<?php
+						$args = array(
+							'theme_location' => 'other-projects',
+							'container' => '',
+							'menu_class' => 'menu',
+						);
+					
+						wp_nav_menu( $args );
+					?>
+				</div>
+			</div>		
+		</footer>
+
+		<?php
+		/**
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+		**/
 		
-		<?php wp_footer(); ?>
+		wp_footer(); ?>
 		
-		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
